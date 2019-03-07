4411b521cb45a76c990e6445709ff8c30b137c42
Crochet Olivier <olivier.crochet@ynov.com>
2019-03-07 13:52:01 +0100

    oupsi le mdp

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://oliviercrochet:lolbite999@cluster0-3n0v9.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://oliviercrochet:<password>@cluster0-3n0v9.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true });
 
 module.exports = {
     mongoose
